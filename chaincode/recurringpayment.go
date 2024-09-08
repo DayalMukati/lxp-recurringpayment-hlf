5be13e02aa342d348834b766c9875f35cf29b4ae
Dayal Mukati <dayalmukati@gmail.com>
2024-09-08 11:55:44 +0530

    Question Updated

--- chaincode/recurringpayment.go
+++ chaincode/recurringpayment.go
@@ -25,142 +25,25 @@ type Subscription struct {
 // CreateSubscription can only be invoked by Org1 users
 func (rpc *RecurringPaymentContract) CreateSubscription(ctx contractapi.TransactionContextInterface, subscriptionID string, payerID string, payeeID string, amount float64, frequency string, totalInstallments int) error {
 	// Verify that the transaction is submitted by Org1
-	clientOrgID, err := ctx.GetClientIdentity().GetMSPID()
-	if err != nil {
-		return fmt.Errorf("failed to get client org: %s", err.Error())
-	}
-	if clientOrgID != "Org1MSP" {
-		return fmt.Errorf("only Org1 can create a subscription")
-	}
-
-	subscription := Subscription{
-		SubscriptionID:   subscriptionID,
-		PayerID:          payerID,
-		PayeeID:          payeeID,
-		Amount:           amount,
-		TotalInstallments: totalInstallments,
-		PaymentsMade:     0,
-		Frequency:        frequency,
-		Completed:        false,
-	}
-
-	subscriptionAsBytes, err := json.Marshal(subscription)
-	if err != nil {
-		return fmt.Errorf("failed to marshal subscription: %s", err.Error())
-	}
-
-	return ctx.GetStub().PutState(subscriptionID, subscriptionAsBytes)
+	// Write the logic to CreateSubscription(subscriptionID, payerID, payeeID, amount, frequency, totalInstallments): This can only be done by Org1 users (payer).
 }
 
 // MakePayment can only be invoked by Org1 users
 func (rpc *RecurringPaymentContract) MakePayment(ctx contractapi.TransactionContextInterface, subscriptionID string) error {
 	// Verify that the transaction is submitted by Org1
-	clientOrgID, err := ctx.GetClientIdentity().GetMSPID()
-	if err != nil {
-		return fmt.Errorf("failed to get client org: %s", err.Error())
-	}
-	if clientOrgID != "Org1MSP" {
-		return fmt.Errorf("only Org1 can make payments")
-	}
-
-	subscriptionAsBytes, err := ctx.GetStub().GetState(subscriptionID)
-	if err != nil {
-		return fmt.Errorf("failed to get subscription: %s", err.Error())
-	}
-
-	if subscriptionAsBytes == nil {
-		return fmt.Errorf("subscription %s does not exist", subscriptionID)
-	}
-
-	subscription := new(Subscription)
-	err = json.Unmarshal(subscriptionAsBytes, subscription)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal subscription: %s", err.Error())
-	}
-
-	if subscription.Completed {
-		return fmt.Errorf("subscription %s is already completed", subscriptionID)
-	}
-
-	// Check if there are remaining installments
-	if subscription.PaymentsMade >= subscription.TotalInstallments {
-		return fmt.Errorf("no remaining installments for subscription %s", subscriptionID)
-	}
-
-	// Increment the number of payments made
-	subscription.PaymentsMade++
-
-	// If all payments are made, mark the subscription as completed
-	if subscription.PaymentsMade >= subscription.TotalInstallments {
-		subscription.Completed = true
-	}
-
-	subscriptionAsBytes, err = json.Marshal(subscription)
-	if err != nil {
-		return fmt.Errorf("failed to marshal subscription: %s", err.Error())
-	}
-
-	return ctx.GetStub().PutState(subscriptionID, subscriptionAsBytes)
+	// Write the logic to MakePayment(subscriptionID): This can only be done by Org1 users (payer).
 }
 
 // ConfirmPayment can only be invoked by Org2 users
 func (rpc *RecurringPaymentContract) ConfirmPayment(ctx contractapi.TransactionContextInterface, subscriptionID string) error {
 	// Verify that the transaction is submitted by Org2
-	clientOrgID, err := ctx.GetClientIdentity().GetMSPID()
-	if err != nil {
-		return fmt.Errorf("failed to get client org: %s", err.Error())
-	}
-	if clientOrgID != "Org2MSP" {
-		return fmt.Errorf("only Org2 can confirm payments")
-	}
-
-	subscriptionAsBytes, err := ctx.GetStub().GetState(subscriptionID)
-	if err != nil {
-		return fmt.Errorf("failed to get subscription: %s", err.Error())
-	}
-
-	if subscriptionAsBytes == nil {
-		return fmt.Errorf("subscription %s does not exist", subscriptionID)
-	}
-
-	// If the payments made are confirmed, just print confirmation
-	subscription := new(Subscription)
-	err = json.Unmarshal(subscriptionAsBytes, subscription)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal subscription: %s", err.Error())
-	}
-
-	fmt.Printf("Payment for subscription %s confirmed by payee.\n", subscriptionID)
-	return nil
+	// Write the logic to ConfirmPayment(subscriptionID): This can only be done by Org2 users (payee).
 }
 
 // QuerySubscriptionStatus can only be invoked by Org2 users
 func (rpc *RecurringPaymentContract) QuerySubscriptionStatus(ctx contractapi.TransactionContextInterface, subscriptionID string) (*Subscription, error) {
 	// Verify that the transaction is submitted by Org2
-	clientOrgID, err := ctx.GetClientIdentity().GetMSPID()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get client org: %s", err.Error())
-	}
-	if clientOrgID != "Org2MSP" {
-		return nil, fmt.Errorf("only Org2 can query the subscription status")
-	}
-
-	subscriptionAsBytes, err := ctx.GetStub().GetState(subscriptionID)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get subscription: %s", err.Error())
-	}
-
-	if subscriptionAsBytes == nil {
-		return nil, fmt.Errorf("subscription %s does not exist", subscriptionID)
-	}
-
-	subscription := new(Subscription)
-	err = json.Unmarshal(subscriptionAsBytes, subscription)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal subscription: %s", err.Error())
-	}
-
-	return subscription, nil
+	// Write the logic to QuerySubscriptionStatus(subscriptionID): This can only be done by Org2 users (payee).
 }
 
 func main() {
